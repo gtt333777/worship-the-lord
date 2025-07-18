348aa8577ee4d08bbe6cbe3ac34fd6a69e4d899d
gtt333777 <gtt333777@gmail.com>
2025-07-18 14:57:04 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-
-// Token stored in base64 format for semi-obfuscation
-const config = {
-  k1: atob("c2wudS5BRjJqZXZpM0pRWXZnY25yTjNHNFRhX1Z6WFBDdU1RR3pTV3VCVjl4dmFUNjJWQXJGdUNXeVk1T2o2TEVBb2tEMVdRM0VDQlk5d1dheXkzcHM5bmVmQkI3WFo2eWtPS3BDaGU=")
-};
