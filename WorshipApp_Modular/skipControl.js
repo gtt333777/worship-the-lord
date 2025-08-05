89ce3cfe4a67fcd51fa0e85dcc4c4d69a86563d8
gtt333777 <gtt333777@gmail.com>
2025-08-05 21:53:46 +0530

    Delete WorshipApp_Modular/skipControl.js

--- WorshipApp_Modular/skipControl.js
+++
@@ -1,6 +0,0 @@
-function skipSeconds(seconds) {
-  const current = Math.max(0, vocalAudio.currentTime + seconds);
-  vocalAudio.currentTime = current;
-  accompAudio.currentTime = current;
-}
-window.skipSeconds = skipSeconds; // Expose globally for button click
