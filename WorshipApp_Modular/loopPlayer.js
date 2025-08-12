49a19b57a66f25eab5f5810b7573bbd5dd826e52
gtt333777 <gtt333777@gmail.com>
2025-08-12 13:30:22 +0530

    Add files via upload

--- WorshipApp_Modular/loopPlayer.js
+++ WorshipApp_Modular/loopPlayer.js
@@ -107,7 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Simulate 3 quick taps to remove vocal sluggishness
           playSegment(segment.start, segment.end, index);
           setTimeout(() => playSegment(segment.start, segment.end, index), 70);
-          setTimeout(() => playSegment(segment.start, segment.end, index), 100);
+          //setTimeout(() => playSegment(segment.start, segment.end, index), 100);
          });
 
          loopButtonsDiv.appendChild(btn);
