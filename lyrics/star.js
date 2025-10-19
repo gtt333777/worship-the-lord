08ea2dce0ee193a5ef17bcda0adcceab11639d6b
gtt333777 <gtt333777@gmail.com>
2025-10-20 04:22:39 +0530

    Add files via upload

---
+++ lyrics/star.js
@@ -0,0 +1,40 @@
+﻿// star.js
+// List of Tamil song names marked as favorites
+
+/*
+window.star = [
+  "அப்பா உம் பாதம் அமர்ந்துவிட்டேன் Appaa um paatham amarnthuvittaen",
+  "அழகாய் திரள் திரளாய் Azhakaay thiral thiralaay",
+  "உமதுமுகம் நோக்கி Umathu mugam nokki",
+  "காக்கும் தெய்வம் இயேசு இருக்க Kakkum deivam yesu iruka"
+];
+*/
+
+// star.js
+//Here you define your song categories using the prefix symbols directly:
+// ---------------------------------------
+// Levels:
+// *** → Green bold (highest)
+// **  → Orange bold
+// *   → Black bold
+// #   → Grey bold
+// ---------------------------------------
+
+window.star = [
+  "* அப்பா உம் பாதம் அமர்ந்துவிட்டேன் Appaa um paatham amarnthuvittaen",
+  "*** அழகாய் திரள் திரளாய் Azhakaay thiral thiralaay",
+  "*** உமதுமுகம் நோக்கி Umathu mugam nokki",
+  "# உன்னதரே என் நேசரே Unnathare en nesarae",
+  "** ஐயா உம் திரு நாமம் Aiyaa um thirunaamam",
+  "*** காக்கும் தெய்வம் இயேசு இருக்க Kakkum deivam yesu iruka",
+  "* தளர்ந்து போன கைகளை திடப்படுத்துங்கள் Thalarnthu pona kaikalai",
+  "* ராஜா உம் மாளிகையில் Raja um maligaiyil",
+  "* விண்ணப்பத்தைக் கேட்பவரே Vinnapathai ketpavare",
+  "* ஜெபம் கேட்டீரையா ஜெயம் Jebam Keteeraiya Jeyam",
+  "* విన్నపాలు విను దైవమా Vinnapalu vinu Daivamaa",
+  "* రాజా నీ భవనములో Raajaa Nee Bhavanamulo",
+
+];
+
+
+
