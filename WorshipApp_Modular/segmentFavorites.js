209a5c40a7faf64016ad73c0149712528e39bd8e
gtt333777 <gtt333777@gmail.com>
2025-11-25 01:56:15 +0530

    Add files via upload

---
+++ WorshipApp_Modular/segmentFavorites.js
@@ -0,0 +1,6 @@
+﻿// segmentFavorites.js
+console.log("segmentFavorites.js loaded");
+
+window.toggleSegmentFavView = function () {
+  alert("⭐ Favorite segments system is now being added step-by-step.");
+};
