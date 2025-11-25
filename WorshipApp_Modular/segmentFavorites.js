5d277e29f2ca03deb18f5c89f53ed55f73d96873
gtt333777 <gtt333777@gmail.com>
2025-11-25 12:54:21 +0530

    Delete WorshipApp_Modular/segmentFavorites.js

--- WorshipApp_Modular/segmentFavorites.js
+++
@@ -1,6 +0,0 @@
-﻿// segmentFavorites.js
-console.log("segmentFavorites.js loaded");
-
-window.toggleSegmentFavView = function () {
-  alert("⭐ Favorite segments system is now being added step-by-step.");
-};
