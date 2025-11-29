46b55f075f3a408a8dc28437604504be17c9190c
gtt333777 <gtt333777@gmail.com>
2025-11-29 18:46:12 +0530

    Add files via upload

--- WorshipApp_Modular/loopPlayer.js
+++ WorshipApp_Modular/loopPlayer.js
@@ -251,11 +251,7 @@ if (window.vocalAudio.currentTime >= endTime - EPS) {
 }
 
 
-
-
-
-
-    } 
+    
     }, 50); // ~20 checks per second
   }).catch(err => {
     console.warn("⚠️ loopPlayer.js: playSegment error:", err);
